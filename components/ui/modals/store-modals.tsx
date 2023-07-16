560f08387e675fbe508d0bdaff142b60672f3032
sbravindran03 <ravindran5211@gmail.com>
2023-07-16 12:37:05 +0530

    initial commit

---
+++ components/ui/modals/store-modals.tsx
@@ -0,0 +1,14 @@
+"use client ";
+import { Modal}  from "@/components/ui/modal";
+export const useStoreModal =()=>{
+    const storeModal =useStoreModal();
+    return(
+   <Modal   title="Create Store"
+   description="Add a new store to manage products and categeories"
+   isOpen={false}
+   onClose={() =>{}}
+  > Future Create Store Form
+   
+   </Modal>
+    );
+};
