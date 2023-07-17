816bbb2989e047383fd350f3dcee0832cc7a92c7
sbravindran03 <ravindran5211@gmail.com>
2023-07-17 22:57:18 +0530

    commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
-"use client ";
-import { Modal } from "@/components/ui/modal";
+"use client"
+import { useStoreModal } from "@/.next/hooks/use-store-modal";
+import { useEffect } from "react";
 const SetupPage =() =>{
+  const  onOpen =useStoreModal((state)=>state.onOpen);
+  const  isOpen =useStoreModal((state)=>state.isOpen);
+
+  useEffect(()=>{
+    if (!isOpen){
+      onOpen();
+    }
+  },[isOpen,onOpen]);
+
     return (
+
      <div className="p-4"> 
-        <Modal title="Test" description="Test Desc" isOpen onClose={()=>{}}>
-          Children
-        </Modal>
+
+       Root Page
 
      </div>
     );
--- components/ui/modals/store-modals.tsx
+++ components/ui/modals/store-modals.tsx
@@ -1,12 +1,13 @@
-"use client ";
+"use client";
+import{useStoreModal} from "@/.next/hooks/use-store-modal";
 import { Modal}  from "@/components/ui/modal";
-export const useStoreModal =()=>{
+export const StoreModal =()=>{
     const storeModal =useStoreModal();
     return(
    <Modal   title="Create Store"
    description="Add a new store to manage products and categeories"
-   isOpen={false}
-   onClose={() =>{}}
+   isOpen={storeModal.isOpen}
+   onClose={storeModal.onClose}
   > Future Create Store Form
    
    </Modal>
